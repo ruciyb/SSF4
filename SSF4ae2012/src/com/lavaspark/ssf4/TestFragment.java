fcdb268af8fb0e40243cb68de8e0666748c0adf0
lei <lei@leitekiMac-mini.local>
2013-04-03 10:35:20 +0800

    0403未知

--- SSF4ae2012/src/com/lavaspark/ssf4/TestFragment.java
+++ SSF4ae2012/src/com/lavaspark/ssf4/TestFragment.java
@@ -7,10 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class TestFragment extends Fragment {
-@Override
-public View onCreateView(LayoutInflater inflater, ViewGroup container,
-		Bundle savedInstanceState) {
-	View v = inflater.inflate(R.layout.test_fragment, container, false);
-	return v;
-}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.test_fragment, container, false);
+		return v;
+	}
 }
